712af35871f2a93479eac3add2ea2945e72f3816
nobleliar9296 <gundeepssidhu@gmail.com>
2026-04-13 21:13:18 -0500

    minor

---
+++ 2202b/main.cpp
@@ -0,0 +1,41 @@
+include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+
+
+
+	int cases;
+
+	cin >> cases;
+
+
+	// get the input and do something with them
+	while (cases > 0) {
+
+		int length = 0;
+
+		cin >> length;
+
+		string now;
+
+		cin >> now;
+
+		
+		// now we have the length of the string and the string itself
+		// we can brute force the solution using recursion but it will be a 2^n solution rather we can 
+		// 
+
+
+
+
+	}
+
+
+	return 0;
+
+
+}
